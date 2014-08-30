e55df5a89b5a313158398f005b8f126f41d9cafd
Jahrain Jackson <jahrain@Jahrains-MacBook-Pro.local>
2014-08-30 22:28:09 +0200

    added pan weapon assets and scripts

---
+++ Assets/Weapon/PanProjectile.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+var startHeight : float = 20.0;
+
+
+private var _killTimer : float = 1;
+private var _killMe : int = 0;
+function Start () {
+	transform.position.y = startHeight;
+	transform.rotation.eulerAngles = Vector3(Random.Range(-30,30),Random.Range(0,360),Random.Range(-60,60));
+	rigidbody.AddForce(0,-100,0);
+}
+
+function Update () {
+	if (_killMe) {
+		_killTimer -= Time.deltaTime;
+		if (_killTimer <= 0) {
+			Destroy(gameObject);
+		}
+	}
+}
+
+function OnCollisionEnter(col: Collision) {
+	Debug.Log(col.gameObject.name);
+	_killMe = 1;
+	_killTimer = 1;
+	
+}
---
+++ Assets/Weapon/PanShooter.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+var panPrefab: Transform;
+private var _shooting = false;
+
+function Start () {
+	Debug.Log("Start");
+}
+
+
+function Update () {
+	if (Input.GetAxis("Fire1")) {
+		if (!_shooting) {
+			_shooting = true;
+			Instantiate(panPrefab, new Vector3(0,0,0), Quaternion.identity);
+		}
+		
+	} else {
+		_shooting = false;
+	}
+}
