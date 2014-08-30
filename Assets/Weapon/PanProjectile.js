e20c86ef8db387f521ce2707a6d19e5af679a6a5
Jahrain <jahrain.jackson@gmail.com>
2014-08-31 00:23:36 +0200

    fixed kill timer for pan

--- Assets/Weapon/PanProjectile.js
+++ Assets/Weapon/PanProjectile.js
@@ -4,7 +4,7 @@ var startHeight : float = 20.0;
 
 private var _killTimer : float = 1;
 private var _killMe : int = 0;
-private var _startTimer : float = 0.1;
+private var _startTimer : float = 1.0;
 function Start () {
 	transform.position.y = startHeight;
 	transform.rotation.eulerAngles = Vector3(Random.Range(-30,30),Random.Range(0,360),Random.Range(-60,60));
@@ -18,13 +18,16 @@ function Update () {
 			Destroy(gameObject);
 		}
 	} else {
-		_startTimer -= Time.deltaTime;
+		
 	}
+	
+	_startTimer -= Time.deltaTime;
 }
 
 function OnCollisionEnter(col: Collision) {
-	Debug.Log(col.gameObject.name);
-	_killMe = 1;
-	_killTimer = 1;
-	
+	if (_startTimer <= 0) {
+		Debug.Log(col.gameObject.name);
+		_killMe = 1;
+		_killTimer = 1;
+	}
 }
