0cdc9672f9d6fe0204bd109df2b8d7af84fd8f0e
Fräntz Miccoli <frantz.miccoli@fakedomain.com>
2014-08-31 05:18:17 +0200

    Now pans with sound ;)

--- Assets/Weapon/PanProjectile.js
+++ Assets/Weapon/PanProjectile.js
@@ -5,6 +5,10 @@ var startHeight : float = 20.0;
 private var _killTimer : float = 1;
 private var _killMe : int = 0;
 private var _startTimer : float = 1.0;
+
+private var AUDIO_CLIP_NAMES = ['pan1', 'pan2', 'pan3', 'pan4'];
+private var _audio_clips = [];
+
 function Start () {
 	transform.position.y = startHeight;
 	transform.rotation.eulerAngles = Vector3(Random.Range(-30,30),Random.Range(0,360),Random.Range(-60,60));
@@ -25,9 +29,17 @@ function Update () {
 }
 
 function OnCollisionEnter(col: Collision) {
-	if (_startTimer <= 0) {
-		Debug.Log(col.gameObject.name);
+	if ((_startTimer <= 0) && (_killMe != 1)) {
 		_killMe = 1;
 		_killTimer = 1;
+		_playSound();
 	}
+	
+}
+
+function _playSound() {
+	var randomIndex : int = this.AUDIO_CLIP_NAMES.length * Random.value;
+	var randomClipName =  this.AUDIO_CLIP_NAMES[randomIndex];
+	this.gameObject.audio.clip = Resources.Load(randomClipName) as UnityEngine.AudioClip;
+	this.gameObject.audio.Play();	
 }
