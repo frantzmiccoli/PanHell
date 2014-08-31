cadf32581fc593209d0ee26d70e21c3a092873fb
Jahrain <jahrain.jackson@gmail.com>
2014-08-31 07:18:46 +0200

    added enemy counter to hud

---
+++ Assets/Map/GameRules.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+private var totalEnemies : int = 0;
+function Start () {
+	var enemies = GameObject.FindGameObjectsWithTag("Enemy");
+	totalEnemies = enemies.length;
+}
+
+function Update () {
+	var enemies = GameObject.FindGameObjectsWithTag("Enemy");
+	totalEnemies = enemies.length;
+	var hudobj : GameObject = GameObject.Find("HudText");
+	var hud = hudobj.GetComponent(GUIText);
+	hud.text = "Total Enemies: "+totalEnemies;
+	if (Input.GetKeyUp(KeyCode.Escape)) {
+		Application.Quit();
+	}
+}
